a9ddf9c284e73f5b957e686e2e37ba22dfb8bd12
Ksenia-Mesh <97454789+Ksenia-Mesh@users.noreply.github.com>
2022-11-06 18:20:58 +0300

    Update task.js

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,52 +1,57 @@
-const formDiv = document.getElementById('signin');
-formDiv.classList.add('signin_active');
+let signinBlock = document.getElementById('signin');
+let signinForm = document.getElementById('signin__form');
+let welcome = document.getElementById('welcome');
+let signinButton = document.getElementById('signin__btn');
+let xhr = new XMLHttpRequest();
 
-const welcome = document.getElementById('welcome');
-const signinBtn = document.getElementById('signin__btn');
-const userId = document.getElementById('user_id');
+signinButton.addEventListener('click', trySignin);
 
-signinBtn.addEventListener('click', sendForm);
+document.addEventListener("DOMContentLoaded", restoreLogin);
 
-function sendForm(event) {
+function sayHello(user) {
+    welcome.classList.add('welcome_active');
+    welcome.innerHTML = `Добро пожаловать, пользователь #<span id="${user}">${user}</span><br><br><button class="btn" id="logout__btn" onclick="localStorage.clear(); window.location.reload();">Выйти</button>`;
+}
+
+function restoreLogin() {
+    let user_id = localStorage.getItem("user_id"); 
+    if (user_id != null) {
+        sayHello(user_id);
+    }
+    else {
+        signinBlock.classList.add('signin_active');
+    }
+}
+
+function trySignin() {
 
     event.preventDefault();
 
-    const form = document.getElementById('signin__form');
-    const formData = new FormData(form);
+    let formData = new FormData(document.forms[0]);
 
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php');
-    xhr.addEventListener('readystatechange', showData);
+    xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/auth.php', true);
+    
     xhr.send(formData);
-
-    function showData(event) {
-
-        if (xhr.readyState === 4 && xhr.status === 200) {
-
-            const response = JSON.parse(xhr.responseText);
-
-            if (response.success) {
-
-                localStorage.userId = response.user_id;
-
-                formDiv.classList.remove('signin_active');
-                welcome.classList.add('welcome_active');
-                userId.innerText = response.user_id;
-
-            } else {
-                alert('Неверный логин/пароль');
+    
+    xhr.onreadystatechange = function() {
+    
+        if (xhr.readyState != 4) return;
+    
+        if (xhr.status != 200) {
+            alert(xhr.status + ': ' + xhr.statusText);
+        }
+        else {
+            let authJSON = JSON.parse(xhr.responseText);
+            if (authJSON.success) {
+                signinBlock.classList.remove('signin_active');
+                sayHello(authJSON.user_id);
+                localStorage.setItem("user_id", authJSON.user_id);
+            }
+            else {
+                signinForm.reset();
+                alert('Неверный логин/пароль!');
             }
         }
+
     }
 }
-
-function init() {
-
-    if (localStorage.userId) {
-        formDiv.classList.remove('signin_active');
-        welcome.classList.add('welcome_active');
-        userId.innerText = localStorage.userId;
-    }
-}
-
-init();
